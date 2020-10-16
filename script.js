ac56af8983e064932f69a5dfe6f860819deb1ad6
Galina Ivanov <galina.ivanov123@gmail.com>
2020-10-16 17:59:43 -0400

    created userPrompt function

--- script.js
+++ script.js
@@ -15,6 +15,29 @@ generateBtn.addEventListener("click", writePassword);
 
 // Generate password 
 function generatePassword() {
+  var userSelection = promptUser()
+  console.log (userSelection);
   return '123';
 
+}
+
+// Generate password 
+function promptUser() {
+  
+  var passwordLenghtStr = prompt ("Please provide password lenght 8 - 128", "10");
+  var passwordLenghtNum = parseInt (passwordLenghtStr);
+
+  var lowerCaseStr = prompt ("Would you like a lowercase letter? y/n", "y");
+  var isLowerCase = lowerCaseStr === "y" || lowerCaseStr === "Y";
+
+  var upperCaseStr = prompt ("Would you like an uppercase letter? Y/N", "Y");
+  var isUpperCase = upperCaseStr === "y" || upperCaseStr === "Y";
+
+  var numeric = prompt ("Would you like a number? y/n", "y");
+  var isNumeric = numeric === "y" || numeric === "Y";
+
+  var specialCharStr = prompt ("Would you like a special character? y/n", "y");
+  var isSpecialChar = specialCharStr === "y" || specialCharStr === "Y";
+
+return {passwordLenghtNum, isLowerCase, isUpperCase, isNumeric, isSpecialChar}
 }
